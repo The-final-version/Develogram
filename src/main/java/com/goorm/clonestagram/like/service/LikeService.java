5f0eda7e0d6897870341864393aa62a80e142f65
Duhikim <macksa29@hotmail.com>
2025-04-07 13:19:42 +0900

    test: 좋아요 리포지토리 단위 테스트 코드 작성
    findByPost_Id, existsByUser_IdAndPost_Id, countByPost_Id, findByUser_IdAndPost_Id
    네 가지 커스텀 메서드에 대한 결과값 테스트코드 작성.
    
    Resolves: #35

--- src/main/java/com/goorm/clonestagram/like/service/LikeService.java
+++ src/main/java/com/goorm/clonestagram/like/service/LikeService.java
@@ -6,7 +6,9 @@ import com.goorm.clonestagram.like.repository.LikeRepository;
 import com.goorm.clonestagram.post.service.PostService;
 import com.goorm.clonestagram.user.domain.Users;
 import com.goorm.clonestagram.user.service.UserService;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,38 +17,36 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class LikeService {
-    private final LikeRepository likeRepository;
-    private final UserService userService;
-    private final PostService postService;
+	private final LikeRepository likeRepository;
+	private final UserService userService;
+	private final PostService postService;
 
-    // 좋아요 토글
-    @Transactional
-    public void toggleLike(Long userId, Long postId) {
-        Users user = userService.findByIdAndDeletedIsFalse(userId);
-        Posts post = postService.findByIdAndDeletedIsFalse(postId);
+	// 좋아요 토글
+	@Transactional
+	public void toggleLike(Long userId, Long postId) {
+		Users user = userService.findByIdAndDeletedIsFalse(userId);
+		Posts post = postService.findByIdAndDeletedIsFalse(postId);
 
-        // userId와 postId를 사용해 좋아요 여부 확인
-        Optional<Like> existingLike = likeRepository.findByUser_IdAndPost_Id(userId, postId);
+		// userId와 postId를 사용해 좋아요 여부 확인
+		Optional<Like> existingLike = likeRepository.findByUser_IdAndPost_Id(userId, postId);
 
-        if (existingLike.isPresent()) {
-            likeRepository.delete(existingLike.get()); // 좋아요 취소
-        } else {
-            Like like = new Like();
-            like.setUser(user);
-            like.setPost(post);
-            likeRepository.save(like); // 좋아요 추가
-        }
-    }
+		if (existingLike.isPresent()) {
+			likeRepository.delete(existingLike.get()); // 좋아요 취소
+		} else {
+			Like like = new Like();
+			like.setUser(user);
+			like.setPost(post);
+			likeRepository.save(like); // 좋아요 추가
+		}
+	}
 
-    @Transactional(readOnly = true)
-    public Long getLikeCount(Long postId) {
-        return likeRepository.countByPost_Id(postId);
-    }
+	@Transactional(readOnly = true)
+	public Long getLikeCount(Long postId) {
+		return likeRepository.countByPost_Id(postId);
+	}
 
-
-    public boolean isPostLikedByLoginUser(Long postId, Long userId) {
-        return likeRepository.existsByUserIdAndPost_Id(userId, postId);
-    }
-
+	public boolean isPostLikedByLoginUser(Long postId, Long userId) {
+		return likeRepository.existsByUser_IdAndPost_Id(userId, postId);
+	}
 
 }
